fbfd0814f152a45ac68981d618a0785bd69e395f
hagaitrg <disahagait@gmail.com>
2021-08-30 09:51:51 +0700

    render view

---
+++ resources/views/latihan1.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+        body{
+        font-family:sans-serif;
+        margin:15%;
+    }
+    </style>
+</head>
+<body>
+    
+    <div class="app">
+        <h1>Hello, Laravel</h1>
+    </div>
+
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
